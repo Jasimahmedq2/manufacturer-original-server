7d984a54ef63c1155f67874d57b30432cfb08591
jasimahmedq2 <jasimahmed90852417>
2022-11-22 12:26:32 +0600

    patch update

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ async function run() {
     //   res.send(result)
     // })
 
-    app.patch('/shipped/:id', verifyJWT, async (req, res) => {
+    app.patch('/shipped/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) }
       const updateDoc = {
